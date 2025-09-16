f172f739e3fbaac7c7c0d14f25fc11cd966455a4
Adam Sahid <masadamsahid@gmail.com>
2025-09-16 09:03:05 +0700

    🚀 feature: add controllers and routes

---
+++ routes/payment/payment.go
@@ -0,0 +1,49 @@
+package routes
+
+import (
+	"payment-service/clients"
+	"payment-service/constants"
+	controllers "payment-service/controllers/http"
+	"payment-service/middlewares"
+
+	"github.com/gin-gonic/gin"
+)
+
+type PaymentRoute struct {
+	controller controllers.IControllerRegistry
+	client     clients.IClientRegistry
+	group      *gin.RouterGroup
+}
+
+type IPaymentRoute interface {
+	Run()
+}
+
+func NewPaymentRoute(
+	group *gin.RouterGroup,
+	controller controllers.IControllerRegistry,
+	client clients.IClientRegistry,
+) IPaymentRoute {
+	return &PaymentRoute{
+		controller: controller,
+		client:     client,
+		group:      group,
+	}
+}
+
+func (p *PaymentRoute) Run() {
+	group := p.group.Group("/payment")
+	group.POST("/webhook", p.controller.GetPayment().Webhook)
+	group.Use(middlewares.Authenticate())
+	group.GET("", middlewares.CheckRole([]string{
+		constants.Admin,
+		constants.Customer,
+	}, p.client), p.controller.GetPayment().GetAllWithPagination)
+	group.GET("/:uuid", middlewares.CheckRole([]string{
+		constants.Admin,
+		constants.Customer,
+	}, p.client), p.controller.GetPayment().GetByUUID)
+	group.POST("", middlewares.CheckRole([]string{
+		constants.Customer,
+	}, p.client), p.controller.GetPayment().Create)
+}
---
+++ routes/registry.go
@@ -0,0 +1,39 @@
+package routes
+
+import (
+	"payment-service/clients"
+	controllers "payment-service/controllers/http"
+	routes "payment-service/routes/payment"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Registry struct {
+	controller controllers.IControllerRegistry
+	group      *gin.RouterGroup
+	client     clients.IClientRegistry
+}
+
+type IRouteRegistry interface {
+	Serve()
+}
+
+func NewRouteRegistry(
+	controller controllers.IControllerRegistry,
+	group *gin.RouterGroup,
+	client clients.IClientRegistry,
+) IRouteRegistry {
+	return &Registry{
+		controller: controller,
+		group:      group,
+		client:     client,
+	}
+}
+
+func (r *Registry) Serve() {
+	r.paymentRoute().Run()
+}
+
+func (r *Registry) paymentRoute() routes.IPaymentRoute {
+	return routes.NewPaymentRoute(r.group, r.controller, r.client)
+}
